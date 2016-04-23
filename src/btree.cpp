8ead2b18359e0b4ee87a430e0897ea3fb98b6167
trotman23 <trotman23@gmail.com>
2016-04-23 00:26:24 -0500

    constructor progress

--- src/btree.cpp
+++ src/btree.cpp
@@ -35,18 +35,41 @@ BTreeIndex::BTreeIndex(const std::string & relationName,
 	//first, check if the index file exists
 	//if the file exists, open it
 	//if not, create a new file
+	//need to do somethi9ngn different for each 3 types of int, double and string
 	cout << "input parameters\n";
 	cout << "relationName: " << relationName;
 	cout << "outIndexName: " << outIndexName;
 	cout << "bufMgrIn: " << bufMgrIn;
 	cout << "attrByteOffset: " << attrByteOffset;
 	cout << "attrType: " << attrType;
+	bufMrg = bufMgrIn;
+	attrByteOffset = attrByteOffset;
+	attributeType = attrType;
+	if (attributeType == 0){
+		//0 = interger attribute type
+		nodeOccupancy = INTARRAYNONLEAFSIZE;
+		leafOccupancy = INTARRAYLEAFSIZE;
+	} 
+	else if (attributeType == 1){
+		//1 = double attribute type
+		nodeOccupancy = DOUBLEARRAYNONLEAFSIZE;
+		leafOccupancy = DOUBLEARRAYLEAFSIZE;
+	} 
+	else if (attributeType == STRING){
+		//2 = string attribute type
+		nodeOccupancy = STRINGARRAYNONLEAFSIZE;
+		leafOccupancy = STRINGARRAYLEAFSIZE;	
+	}
+	else {
+		//should never occer
+	}
 	try {
+		file = new BlobFile(outIndexName, false);what a
 		//try to find the file?
 
 	}catch (FileNotFoundException fnfe){
-		//create the new file
-		File file = new BlobFile(outIndexName, true);
+		//create the new file, which is declared in the header btree.h
+		file = new BlobFile(outIndexName, true);
 	}
 }
 
